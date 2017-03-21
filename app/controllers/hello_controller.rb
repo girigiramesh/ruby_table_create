b982f3a65001dbb3780520a34181a22ce2adb7ec
Ramesh Girigi <ramesh.kvana@gmail.com>
2017-03-21 19:52:27 +0530

    rendering json format

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,7 @@
 class HelloController < ApplicationController
   def hello
     puts "Hello World..........."
+    @user=User.all
+    render json:{:status => 'success', :result => @user, :message => 'your are sign in successfully'}, status: 200
   end
 end
