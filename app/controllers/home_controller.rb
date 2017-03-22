767224e270ba570e7b8b05e6f1ea00e49b6d1ca3
Ramesh Girigi <ramesh.kvana@gmail.com>
2017-03-22 16:02:46 +0530

    rendering json format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
-class HelloController < ApplicationController
-  def hello
-    puts "Hello World..........."
+class HomeController < ApplicationController
+  def home
+  end
+  def users
     @user=User.all
     render json:{:status => 'success', :result => @user, :message => 'your are sign in successfully'}, status: 200
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'hello#hello'
-  get '/hello', to: 'hello#hello'
+  root 'home#home'
+  get '/home', to:'home#home'
+  get '/users', to: 'home#users'
 end
